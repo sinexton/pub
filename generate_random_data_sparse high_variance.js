38b0861ed0b50a4c4505824c7d1f200f3cba7aa2
Andras Horvath <mail@log69.com>
2020-05-12 17:20:54 +0200

    update

--- generate_random_data_sparse high_variance.js
+++ generate_random_data_sparse high_variance.js
@@ -3,16 +3,16 @@
 var n = 7;
 var temp = [];
 var i;
-for (i = 0; i < n; i++) {
+for ( i = 0; i < n; i++ ) {
 	var i2;
-	for (i2 = 0; i2 < Math.pow(2,i); i2++) {
-		var r = Math.floor( Math.random() * Math.pow(2,n-i) );
+	for ( i2 = 0; i2 < Math.pow( 2, i ); i2++ ) {
+		var r = Math.floor( Math.random() * Math.pow( 2, n-i ));
 		temp.push( r );
 	}
 }
 var result = [];
 while ( temp.length > 0 ){
-	var i = Math.floor(Math.random() * temp.length );
+	var i = Math.floor( Math.random() * temp.length );
 	var v = temp.splice( i, 1 )[0];
 	result.push( v );
 }
